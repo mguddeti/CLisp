e5fda7c40783dd29185a172d145339436085244e
Suhas44 <suhas8@outlook.com>
2022-02-20 12:24:39 -0600

    Enhanced S-Expressions

--- parsing.c
+++ parsing.c
@@ -11,18 +11,22 @@
 #endif
 
 /* Declare New lval Struct */
-typedef struct
-{
+typedef struct lval {
     int type;
     long num;
-    int err;
+    /* Error and Symbol types have some string data */
+    char *err;
+    char *sym;
+    /* Count and Pointer to a list of "lval*" */
+    int count;
+    struct lval **cell;
 } lval;
 
 /* Create Enumeration of Possible lval Types */
-enum {
-    LVAL_NUM,
-    LVAL_ERR
-};
+enum { LVAL_ERR,
+       LVAL_NUM,
+       LVAL_SYM,
+       LVAL_SEXPR };
 
 /* Create Enumeration of Possible Error Types */
 enum {
@@ -44,8 +48,11 @@ char *readline(char *prompt) {
 
 int number_of_nodes(mpc_ast_t *t);
 lval eval(mpc_ast_t *t);
-lval lval_num(long x);
-lval lval_err(int x);
+lval *lval_num(long x);
+lval *lval_err(char *m);
+lval *lval_sym(char *s);
+lval *lval_sexpr(void);
+void lval_del(lval *v);
 void lval_print(lval v);
 void lval_println(lval v);
 lval eval_op(lval x, char *op, lval y);
@@ -131,19 +138,38 @@ lval eval(mpc_ast_t *t) {
     return x;
 }
 
-/* Create a new number type lval */
-lval lval_num(long x) {
-    lval v;
-    v.type = LVAL_NUM;
-    v.num = x;
+/* Construct a pointer to a new Number lval */
+lval *lval_num(long x) {
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_NUM;
+    v->num = x;
     return v;
 }
 
-/* Create a new error type lval */
-lval lval_err(int x) {
-    lval v;
-    v.type = LVAL_ERR;
-    v.err = x;
+/* Construct a pointer to a new Error lval */
+lval *lval_err(char *m) {
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_ERR;
+    v->err = malloc(strlen(m) + 1);
+    strcpy(v->err, m);
+    return v;
+}
+
+/* Construct a pointer to a new Symbol lval */
+lval *lval_sym(char *s) {
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_SYM;
+    v->sym = malloc(strlen(s) + 1);
+    strcpy(v->sym, s);
+    return v;
+}
+
+/* A pointer to a new empty Sexpr lval */
+lval *lval_sexpr(void) {
+    lval *v = malloc(sizeof(lval));
+    v->type = LVAL_SEXPR;
+    v->count = 0;
+    v->cell = NULL;
     return v;
 }
 
@@ -205,4 +231,32 @@ lval eval_op(lval x, char *op, lval y) {
     }
 
     return lval_err(LERR_BAD_OP);
+}
+
+void lval_del(lval *v) {
+    switch (v->type) {
+        /* Do nothing special for number type */
+        case LVAL_NUM:
+            break;
+
+        /* For Err or Sym free the string data */
+        case LVAL_ERR:
+            free(v->err);
+            break;
+        case LVAL_SYM:
+            free(v->sym);
+            break;
+
+        /* If Sexpr then delete all elements inside */
+        case LVAL_SEXPR:
+            for (int i = 0; i < v->count; i++) {
+                lval_del(v->cell[i]);
+            }
+            /* Also free the memory allocated to contain the pointers */
+            free(v->cell);
+            break;
+    }
+
+    /* Free the memory allocated for the "lval" struct itself */
+    free(v);
 }
